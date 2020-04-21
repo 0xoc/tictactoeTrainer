1a57ce68290ce1a1fad4238ef2d93e89b433a2f5
Ali Parvizi <snparvizi75@gmail.com>
2020-04-21 23:25:01 +0430

    complete mcts algorithm implementation

--- ai.py
+++ ai.py
@@ -1,19 +1,35 @@
+from abc import ABC
+
 from game import Game, x, o
 import copy
 import random
 import math
 
 
-class RootNode:
+class AbstractNode(ABC):
+    def __init__(self):
+        self.parent = None
+        self.action = None
+        self.children = []
+        self.weight = 0
+        self.simulations = 0
+
+
+class RootNode(AbstractNode):
+    """A root node for the MCTS algorithm"""
+
     def __init__(self, game_state: Game):
+        super().__init__()
         self.game_state = copy.deepcopy(game_state)
         self.children = []
 
 
-class Node:
-    def __init__(self, game: Game, parent, action):
+class Node(AbstractNode):
+    """A child node in MCTS tree"""
+
+    def __init__(self, parent, action):
+        super().__init__()
         self.action = action
-        self.game: Game = copy.deepcopy(game)
         self.parent = parent
         self.weight = 0
         self.simulations = 0
@@ -23,75 +39,96 @@ class Node:
         return self.weight / self.simulations
 
 
-ai_playing_as = o
+class MCTS:
+    """Wrapper for MCTS algorithm at a specific game state"""
 
+    def __init__(self, game_state: Game, playing_as, max_iterations=10000):
+        self.root = RootNode(game_state)
+        self.max_iterations = max_iterations
+        self.playing_as = playing_as
 
-def traverse(root: Node) -> Node:
-    node = root
+    def get_game_state(self, node: Node) -> Game:
+        """Generate the game state at the given node"""
 
-    while len(node.children):
-        node = ucb_best(node.children)
+        path = []
+        current = node
+        while current is not None and current.action is not None:
+            path.append(current.action)
+            current = current.parent
+        base_game = copy.deepcopy(self.root.game_state)
+        game_state = Game.chain_result(base_game, path[::-1])
 
-    expand(node)
+        return game_state
 
-    if len(node.children):
-        return node.children[0]
+    def traverse(self) -> Node:
+        """Return the node to start a play out from"""
+        node = self.root
 
-    return node
+        while len(node.children):
+            node = self.ucb_best(node.children)
 
+        self.expand(node)
 
-def play_out(game: Game):
-    terminated, the_winner = game.terminal()
+        if len(node.children):
+            return node.children[0]
 
-    if terminated:
-        if the_winner is None:
-            return 0
-        elif the_winner == ai_playing_as:
-            return 1
-        else:
-            return -1
+        return node
 
-    return play_out(Game.result(game, random.choice(game.actions())))
+    def play_out(self, game: Game):
+        """Play out the game with a random selection policy"""
+        terminated, the_winner = game.terminal()
 
+        if terminated:
+            if the_winner is None:
+                return 0
+            elif the_winner == self.playing_as:
+                return 1
+            else:
+                return -1
 
-def propagate(node, result):
-    node.weight += result
-    node.simulations += 1
+        return self.play_out(Game.result(game, random.choice(game.actions())))
 
-    if node.parent:
-        propagate(node.parent, result)
+    def update_and_propagate(self, node, result):
+        """Update the current node stats with the given result, and propagate"""
+        node.weight += result
+        node.simulations += 1
 
+        if node.parent:
+            self.update_and_propagate(node.parent, result)
 
-def expand(node):
-    for action in node.game.actions():
-        n = Node(Game.result(node.game, action), node, action)
-        node.children.append(n)
+    def expand(self, node):
+        """Expand the given node by all possible actions"""
+        game_state = self.get_game_state(node)
 
+        for action in game_state.actions():
+            n = Node(node, action)
+            node.children.append(n)
 
-def ucb(node):
-    return node.get_avg() + math.sqrt(2) * math.sqrt(math.log(node.parent.simulations) / node.simulations)
+    @staticmethod
+    def ucb(node):
+        """Calculate the UCB1 value for the given node"""
+        return node.get_avg() + math.sqrt(2) * math.sqrt(math.log(node.parent.simulations) / node.simulations)
 
+    def ucb_best(self, nodes) -> Node:
+        """Return the node with highest UCB1 value"""
+        for node in nodes:
+            if node.simulations == 0:
+                return node
 
-def ucb_best(nodes) -> Node:
-    for node in nodes:
-        if node.simulations == 0:
-            return node
+        return max(nodes, key=lambda n: self.ucb(n))
 
-    return max(nodes, key=lambda n: ucb(n))
+    def mcts(self):
+        """Apply the MCTS algorithm from the root and return the best action"""
 
+        for i in range(self.max_iterations):
+            nxt_node = self.traverse()
+            game_state = self.get_game_state(nxt_node)
+            node_value = self.play_out(game_state)
+            self.update_and_propagate(nxt_node, node_value)
 
-def mcts(game):
-    root = Node(game, None, None)
-    max_generations = 10000
+            if i % 100 == 0:
+                print(str(i) + "/" + str(self.max_iterations), end='\r')
 
-    for i in range(max_generations):
-        nxt_node = traverse(root)
-        node_value = play_out(nxt_node.game)
-        propagate(nxt_node, node_value)
+        best_node = max(self.root.children, key=lambda n: n.get_avg())
 
-        if i % 100 == 0:
-            print(str(i) + "/" + str(max_generations), end='\r')
-
-    best_node = max(root.children, key=lambda n: n.get_avg())
-
-    return best_node.action, root
+        return best_node.action
--- runner.py
+++ runner.py
@@ -2,23 +2,28 @@ import jsonpickle
 from ast import literal_eval as make_tuple
 
 from game import Game, x, o
-from ai import mcts
-game = Game(7, 4, x)
+from ai import MCTS
+
+
+game = Game(5, 4, x)
 
 run = True
-
-file = open("learn_data/learned.json", 'w')
 
 while run:
     game.print_board()
     i, j = make_tuple(input("Enter row, col: "))
     game.move_to(i, j)
     game.print_board()
-    print("Waiting for AI move ...")
-    ai_move, root = mcts(game)
-    file.write(jsonpickle.encode(root))
-    file.close()
+
+    finished, winner = game.terminal()
+    if finished:
+        run = False
+
+    print("Waiting for AI move ... ")
+    agent = MCTS(game_state=game, playing_as=o)
+    ai_move = agent.mcts()
     game.move_to(ai_move[0], ai_move[1])
+
     finished, winner = game.terminal()
     if finished:
         run = False
